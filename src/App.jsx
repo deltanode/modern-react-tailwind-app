c2b82b8209c0221071cd5f5d27ce93b11162aeb5
Yogesh <yogesh7132@gmail.com>
2023-05-06 17:50:34 +0530

    Fixed: typo

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,23 @@
-import styles from "./style"
+import style from "./style"
 
 import { Navbar, Hero, Stats, Business, Billing, CardDeal, Testimonials, Clients, CTA, Footer } from "./components"
 
 const App = () => (
   <div className="bg-primary w-full overflow-hidden">
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
+    <div className={`${style.paddingX} ${style.flexCenter}`}>
+      <div className={`${style.boxWidth}`}>
         <Navbar />
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
+    <div className={`bg-primary ${style.flexStart}`}>
+      <div className={`${style.boxWidth}`}>
         <Hero />
       </div>
     </div>
 
-    <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
-      <div className={`${styles.boxWidth}`}>
+    <div className={`bg-primary ${style.paddingX} ${style.flexStart}`}>
+      <div className={`${style.boxWidth}`}>
         <Stats />
         <Business />
         <Billing />
