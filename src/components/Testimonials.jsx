203e89ddf842755e6ed81e6d26d91cb9271ebec2
Yogesh <yogesh7132@gmail.com>
2023-05-07 17:49:54 +0530

    Add Testimonials Section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,7 +1,27 @@
-import React from "react"
+import { feedback } from "../constants"
+import style from "../style"
+import FeedbackCard from "./FeedbackCard"
 
-const Testimonials = () => {
-  return <div>Testimonials</div>
-}
+const Testimonials = () => (
+  <section id="clients" className={`${style.paddingY} ${style.flexCenter} flex-col relative`}>
+    {/* gradient */}
+    <div className="absolute z-[0] w-[60%] h-[60%] -right-[50%] rounded-full blue__gradient" />
+
+    <div className="w-full flex justify-between items-center md:flex-row flex-col sm:mb-16 mb-6 relative z-[1]">
+      <h2 className={style.heading2}>
+        What people are <br className="sm:block hidden" /> saying about us
+      </h2>
+      <div className="w-full md:mt-0 mt-6">
+        <p className={`${style.paragraph} max-w-[450px] text-left`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+      </div>
+    </div>
+
+    <div className="flex flex-wrap sm:justify-start justify-center w-full feedback-conatiner relative z-[1]">
+      {feedback.map(card => (
+        <FeedbackCard key={card.id} {...card} />
+      ))}
+    </div>
+  </section>
+)
 
 export default Testimonials
