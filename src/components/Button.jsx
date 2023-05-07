26f82a98439542b749d38fa84883d1a198d81b86
Yogesh <yogesh7132@gmail.com>
2023-05-07 15:58:55 +0530

    Add Billing Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 
-const Button = () => {
-  return <div>Button</div>
+const Button = ({ style }) => {
+  return (
+    <button type="button" className={`${style} py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none`}>
+      Get Started
+    </button>
+  )
 }
 
 export default Button
