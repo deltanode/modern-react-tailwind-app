3f284a88011c77f8ac5f638638f5f572103eed6d
Yogesh <yogesh7132@gmail.com>
2023-05-07 16:47:23 +0530

    Add Carddeal Section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const Button = ({ style }) => {
   return (
-    <button type="button" className={`${style} py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none`}>
+    <button type="button" className={`${style} py-4 px-6 bg-blue-gradient font-poppins font-medium text-[18px] text-primary outline-none rounded-[10px]`}>
       Get Started
     </button>
   )
