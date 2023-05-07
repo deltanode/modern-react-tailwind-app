2b2dbccacb3859ddfa444e4f0cb3569ff4892a7b
Yogesh <yogesh7132@gmail.com>
2023-05-07 18:24:51 +0530

    Add CTA:Call to Action Section

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,7 +1,17 @@
-import React from "react"
+import style from "../style"
+import Button from "./Button"
 
-const CTA = () => {
-  return <div>CTA</div>
-}
+const CTA = () => (
+  <section className={` ${style.flexCenter}  ${style.marginY} ${style.padding} sm:flex-row flex-col bg-black-gradient-2 rounded-[20px] box-shadow`}>
+    <div className="flex flex-col flex-1">
+      <h2 className={style.heading2}>Let’s try our service now!</h2>
+      <p className={`${style.paragraph} max-w-[470px] mt-5`}>Everything you need to accept card payments and grow your business anywhere on the planet.</p>
+    </div>
+
+    <div className={`${style.flexCenter} sm:ml-10 ml-0 sm:mt-0 mt-10`}>
+      <Button />
+    </div>
+  </section>
+)
 
 export default CTA
--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -5,7 +5,7 @@ const Clients = () => (
   <section className={`${style.flexCenter} my-4`}>
     <div className={`${style.flexCenter} flex-wrap w-full`}>
       {clients.map(client => (
-        <div key={HTMLDataListElement.id} className={`flex-1 ${style.flexCenter} sm:w-[192px] w-[100px]`}>
+        <div key={client.id} className={`flex-1 ${style.flexCenter} sm:w-[192px] w-[100px]`}>
           <img src={client.logo} alt="client" className="sm:w-[192px] w-[100px] object-contain" />
         </div>
       ))}
